938ac4275e33cd9f524bd16d4a6541fb9c87ba3f
Jackson Yeager <jacksonyeager@gmail.com>
2021-04-06 14:43:20 -0400

    Added logs

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
     try {
+        console.log('REQUEST', request);
         response.send(request.body);
     } catch (e) {
         response.status(400).send("Cannot parse the request body!");
